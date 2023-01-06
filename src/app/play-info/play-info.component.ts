c7058e4244e9282ce3a38304667c7a8ef4cf7c24
Jordan <JordanAlexWatt@hotmail.com>
2023-01-05 17:33:33 -0800

    Add landing page rework

--- src/app/play-info/play-info.component.ts
+++ src/app/play-info/play-info.component.ts
@@ -25,6 +25,25 @@ export class PlayInfoComponent {
       icon: "DEMONKIND",
       quote: "I played my character Bazgalith for a year or more, these other fools just didn't realize that there is a" +
         " ring of sacrifice. (Also you can have a cat companion - what's not to love?!)"
+    },
+    {
+      name: "Starr 'Dragonfly' Melton",
+      icon: "DRAGONKIND",
+      quote: "What started off as a homebrew D&D system quickly turned into an official system that many can enjoy." +
+      " The insane amount of TLC that went into this really shows. When starting, it can be a bit intimating with the amount" +
+      " of detail, but once you get past that you'll see how incredible this is. I have played this for a couple years," +
+      " and even though I lost count on how many character I've went through, there was never a dull moment creating my" +
+      "characters and playing the story. Dark Souls gameplay experience not required."
+    },
+    {
+      name: "50 drums",
+      icon: "CORVIAN",
+      quote: "Tim has created not only a unique and amazing play system from the ground up, he's also developed a whole world \
+      with rich history and so many mind bending mysteries around every corner. As someone who enjoys the Dark Souls games and \
+      D&D, this is a beautiful fusing of the two. I love how strategic the play style is, how collaborative with the other \
+      players, and the mechanics of healing, AP & FP to manage in combat. You can also be very creative with your own character \
+      creation as there are seemingly infinite combinations with the different races, starting background, weapons and feats. \
+      10/10 highly recommend for anyone who is interested in delving way too deep... and too greedily."
     }
   ];
 }
