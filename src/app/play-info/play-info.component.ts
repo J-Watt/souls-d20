a88602a73731bc785502df45bc9d48ae5d1da14d
Jordan <JordanAlexWatt@hotmail.com>
2023-01-20 12:18:15 -0800

    Move testimonials to also be in support

--- src/app/play-info/play-info.component.ts
+++ src/app/play-info/play-info.component.ts
@@ -8,21 +8,10 @@ import { Component } from '@angular/core';
 export class PlayInfoComponent {
   testimonials = [
     {
-      name: "Zand, longtime player, most deaths in a campaign",
-      icon: "GRME",
-      quote: "Very few systems let me kill my character every week, then create a new one that is wildly different."
-    },
-    {
-      name: "Kelly",
-      icon: "FERNO",
-      quote: "So Random! I never know what fresh Hell my toon will fall into each session. It really keeps you on your toes!" +
-        " Never get attached to your toon, or build a very thorough character backstory - you're wasting your time."
-    },
-    {
-      name: "Liz",
-      icon: "DEMONKIND",
-      quote: "I played my character Bazgalith for a year or more, these other fools just didn't realize that there is a" +
-        " ring of sacrifice. (Also you can have a cat companion - what's not to love?!)"
+      name: "Grant 'Showers' Delhaes",
+      icon: "DRAGONKIND",
+      quote: "The Souls D20 system has been the perfect intro for a first-time TTRPG player. It has the depth to allow for \
+      incredible freedom and problem solving, as well as incredible stupidity, and I couldn't ask for anything more."
     },
     {
       name: "Starr 'Dragonfly' Melton",
@@ -42,12 +31,6 @@ export class PlayInfoComponent {
       players, and the mechanics of healing, AP & FP to manage in combat. You can also be very creative with your own character \
       creation as there are seemingly infinite combinations with the different races, starting background, weapons and feats. \
       10/10 highly recommend for anyone who is interested in delving way too deep... and too greedily."
-    },
-    {
-      name: "Grant 'Showers' Delhaes",
-      icon: "DRAGONKIND",
-      quote: "The Souls D20 system has been the perfect intro for a first-time TTRPG player. It has the depth to allow for \
-      incredible freedom and problem solving, as well as incredible stupidity, and I couldn't ask for anything more."
     }
   ];
 
--- src/app/support/support.component.ts
+++ src/app/support/support.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./support.component.css']
 })
 export class SupportComponent {
-
+  testimonials = [
+    {
+      name: "Zand, longtime player, most deaths in a campaign",
+      icon: "GRME",
+      quote: "Very few systems let me kill my character every week, then create a new one that is wildly different."
+    },
+    {
+      name: "Kelly",
+      icon: "FERNO",
+      quote: "So Random! I never know what fresh Hell my toon will fall into each session. It really keeps you on your toes!" +
+        " Never get attached to your toon, or build a very thorough character backstory - you're wasting your time."
+    },
+    {
+      name: "Liz",
+      icon: "DEMONKIND",
+      quote: "I played my character Bazgalith for a year or more, these other fools just didn't realize that there is a" +
+        " ring of sacrifice. (Also you can have a cat companion - what's not to love?!)"
+    }
+  ];
 }
